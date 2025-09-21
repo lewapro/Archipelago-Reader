50fbf1b76f4b030360b262ff20373050465a8a94
lewapro <50249720+lewapro@users.noreply.github.com>
2025-09-21 23:51:24 +0300

    Delete Archipelago Reader/NamesReader/dist directory

--- Archipelago Reader/NamesReader/dist/config.py
+++
@@ -1,7 +0,0 @@
-# Application settings
-SERVER_URI = "ws://localhost:38281"
-PLAYER_NAME = "lewapro"
-PASSWORD = ""
-GAME = "Manual_TeamFortress2_GP"
-TARGET_PLAYERS = ['lewapro', 'man']
-MAX_MESSAGE_SIZE = 10485760
